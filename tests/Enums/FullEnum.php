8303bd43180fcab7ff8c3f8849fc7c306a957000
Eugene Palshin <eo.palshin@gmail.com>
2020-11-09 23:42:44 +0300

    Add class for test

---
+++ tests/Enums/FullEnum.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Palshin\Enum\Tests\Enums;
+
+use Palshin\Enum\Enum;
+
+/**
+ * @method static self A()
+ * @method static self B()
+ */
+class FullEnum extends Enum
+{
+  protected static function values(): array
+  {
+    return [
+      'A' => 'FOO',
+      'B' => 'BAR',
+    ];
+  }
+
+  public static function labels(): array
+  {
+    return [
+      'A' => 'A Label',
+      'B' => 'B Label',
+    ];
+  }
+
+  public static function descriptions(): array
+  {
+    return [
+      'A' => 'A description',
+      'B' => 'B description',
+    ];
+  }
+}
